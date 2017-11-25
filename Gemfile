81fbb92e8c79d74ec37642b2875be4510d1e7de9
1412320 <quocminh97@gmail.com>
2017-11-25 13:52:50 +0700

    Set up typescript

--- Gemfile
+++ Gemfile
@@ -36,5 +36,4 @@ group :production do
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
 gem 'mini_racer', platforms: :ruby
---
+++ app/javascript/Login/LoginBox.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default class LoginBox extends React.Component {
+  
+  render() {
+    return(
+      <div className="login-box">
+        <h3 className="center login-title"><strong>Ngon</strong></h3>
+      </div>
+    );
+  }
+}
